92bffb889d6511e3e55bd681feb62ecbd95e1856
kassstg <kim.trujillo924@gmail.com>
2024-08-10 01:06:56 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const yesBtn = document.querySelector('#yesBtn');
+yesBtn.addEventListener('click',function() {
+    alert('Me alegra que hayas dicho que si, te amo y quiero que nos amemos asi muchito con bastante por mucho tiempo mas mi amor muak')
+});
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('mouseover',function( ) {
+const randomX = parseInt(Math.random()*100);
+const randomY = parseInt(Math.random()*100);
+noBtn.style.setProperty('top',randomY+'%');
+noBtn.style.setProperty('left',randomX+'%');
+noBtn.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%)`);
+})
